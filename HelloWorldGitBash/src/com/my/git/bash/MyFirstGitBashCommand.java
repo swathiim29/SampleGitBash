690cc2ad0b0976867797f34ebb101130d1208d6f
venmanda <mvswathi446@gmail.com>
2018-06-26 18:53:34 +0530

    commiting my project

---
+++ HelloWorldGitBash/src/com/my/git/bash/MyFirstGitBashCommand.java
@@ -0,0 +1,12 @@
+package com.my.git.bash;
+
+public class MyFirstGitBashCommand {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("my first gitbash command is git status");
+
+	}
+
+}
